6070488fb5335e69f7786ff5e06bcb424fd4b3c2
Tux <TuxGit@users.noreply.github.com>
2019-07-31 10:01:59 +0300

    Add edit mode and a bit of logic

--- app/src/main/java/ru/skillbranch/devintensive/ui/profile/ProfileActivity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/ui/profile/ProfileActivity.kt
@@ -1,17 +1,99 @@
 package ru.skillbranch.devintensive.ui.profile
 
+import android.graphics.ColorFilter
+import android.graphics.PorterDuff
+import android.graphics.PorterDuffColorFilter
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_profile.*
 import ru.skillbranch.devintensive.R
 
 class ProfileActivity : AppCompatActivity() {
+    companion object {
+        const val IS_EDIT_MODE = "IS_EDIT_MODE"
+    }
+
+    var isEditMode = false
+    lateinit var viewFields: Map<String, TextView>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+        initViews(savedInstanceState)
 
         Log.d("M_MainActivity", "onCreate")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+        outState?.putBoolean(IS_EDIT_MODE, isEditMode)
+
+        Log.d("M_MainActivity", "onSaveInstanceState")
+    }
+
+    private fun initViews(savedInstanceState: Bundle?) {
+        viewFields = mapOf(
+            "nickName" to tv_nick_name,
+            "rank" to tv_rank,
+            "firstName" to et_first_name,
+            "lastName" to et_last_name,
+            "about" to et_about,
+            "repository" to et_repository,
+            "rating" to tv_rating,
+            "respect" to tv_respect
+        )
+
+        isEditMode = savedInstanceState?.getBoolean(IS_EDIT_MODE, false) ?: false
+        showCurrentMode(isEditMode)
+
+        // btn_edit.setOnClickListener(object: View.OnClickListener{
+        //     override fun onClick(v: View?) {
+        //         _TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        //     }
+        // })
+        btn_edit.setOnClickListener {
+            isEditMode = !isEditMode
+            showCurrentMode(isEditMode)
+        }
+    }
+
+    private fun showCurrentMode(isEditMode: Boolean) {
+        val info = viewFields.filter { setOf("firstName", "lastName", "about", "repository").contains(it.key) }
+        // (key, value)
+        for ((_, v) in info) {
+            v as EditText
+            v.isFocusable = isEditMode
+            v.isFocusableInTouchMode = isEditMode
+            v.isEnabled = isEditMode
+            v.background.alpha = if (isEditMode) 255 else 0
+        }
+
+        ic_eye.visibility = if (isEditMode) View.GONE else View.VISIBLE
+        wr_about.isCounterEnabled = isEditMode
+
+        with(btn_edit) {
+            val filter: ColorFilter? = if (isEditMode) {
+                PorterDuffColorFilter(
+                    resources.getColor(R.color.color_accent, theme),
+                    PorterDuff.Mode.SRC_IN
+                )
+            } else {
+                null
+            }
+
+            val icon = if (isEditMode) {
+                resources.getDrawable(R.drawable.ic_save_black_24dp)
+            } else {
+                resources.getDrawable(R.drawable.ic_edit_black_24dp)
+            }
+
+            background.colorFilter = filter
+            setImageDrawable(icon)
+        }
     }
 
     override fun onRestart() {
@@ -44,9 +126,4 @@ class ProfileActivity : AppCompatActivity() {
         Log.d("M_MainActivity", "onDestroy")
     }
 
-    override fun onSaveInstanceState(outState: Bundle?) {
-        super.onSaveInstanceState(outState)
-
-        Log.d("M_MainActivity", "onSaveInstanceState")
-    }
 }
